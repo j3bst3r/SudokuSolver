3d7577c40188cf453e2cba16db4ef5f8a31e73af
jebbrown <83508737+jebbbyyy@users.noreply.github.com>
2024-12-19 00:42:27 -0500

    remove solver.h for now. init check_board_is_valid

--- solver.c
+++ solver.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#include "solver.h"
+const int ROW_LENGTH = 9;
+const int COLUMN_LENGTH = 9;
+const int PUZZLE_LENGTH = ROW_LENGTH*COLUMN_LENGTH;
+const int NUMBER_OF_BOXES = 9;
+const int BOX_LENGTH;
+
+typedef struct {
+    double guesses[PUZZLE_LENGTH];
+} board;
 
 void read_clues(int* clue_info, int num_clues) {
     for (int i=0; i<num_clues; i++) {
@@ -10,6 +19,54 @@ void read_clues(int* clue_info, int num_clues) {
             exit(1);
         }
     }
+}
+
+bool check_board_is_valid(board b) {
+    //check all rows are valid
+    for (int row=0; row<ROW_LENGTH; row++) {
+        int counts[ROW_LENGTH] = { 0 };
+        for (int col=0; col<COLUMN_LENGTH; col++) {
+            int guess = b.guesses[row*ROW_LENGTH + col];
+            if (guess != -1) {
+                counts[guess]++;
+            }
+            if (counts[guess] > 1) {
+                return false;
+            }
+        }
+    }
+
+    //check all columns are valid
+    for (int col=0; col<COLUMN_LENGTH; col++) {
+        int counts[COLUMN_LENGTH] = { 0 };
+        for (int row=0; row<ROW_LENGTH; row++) {
+            int guess = b.guesses[row*ROW_LENGTH + col];
+            if (guess != -1) {
+                counts[guess]++;
+            }
+            if (counts[guess] > 1) {
+                return false;
+            }
+        }
+    }
+
+    //check all 3x3 box's are valid
+    for (int box=0; box<NUMBER_OF_BOXES; box++) {
+        for (int row=0; row<BOX_LENGTH; row++) {
+            int counts[NUMBER_OF_BOXES] = { 0 };
+            for (int col=0; col<BOX_LENGTH; col++) {
+                int guess = b.guesses[row*ROW_LENGTH + col];
+                if (guess != -1) {
+                    counts[guess]++;
+                }
+                if (counts[guess] > 1) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
 }
 
 int main() {
--- solver.h
+++
@@ -1,6 +0,0 @@
-#ifndef SOLVER_H
-#define SOLVER_H
-
-void read_clues(int* clue_info, int num_clues);
-
-#endif
