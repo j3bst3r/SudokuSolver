41183da70341aec139d376e82355204618b90f11
jebbrown <83508737+jebbbyyy@users.noreply.github.com>
2024-12-20 15:14:08 -0500

    add in a clue_indices array and num_clues so we know which indices not to change.

--- solver.c
+++ solver.c
@@ -8,13 +8,18 @@ const int PUZZLE_LENGTH = ROW_LENGTH*COLUMN_LENGTH;
 const int NUMBER_OF_BOXES = 9;
 const int BOX_LENGTH;
 
-void read_clues(int* guesses) {
+void read_clues(int* guesses, int* clue_indices, int* num_clues) {
 
+    *num_clues = 0;
     for (int row=0; row<ROW_LENGTH; row++) {
         for (int col=0; col<COLUMN_LENGTH; col++) {
             if (scanf("%d", &guesses[row*ROW_LENGTH+col]) != 1) {
                 printf("error reading clue at position (row, col) = %d, %d", row, col);
                 exit(1);
+            }
+            if (guesses[row*ROW_LENGTH+col] != 0) {
+                clue_indices[*num_clues] =  row*ROW_LENGTH+col;
+                *num_clues++;
             }
         }
     }
@@ -71,24 +76,35 @@ bool check_board_is_valid(int* guesses) {
 
 }
 
-int main() {
-    int guesses[PUZZLE_LENGTH];
-    read_clues(guesses);
+bool check_row_is_valid(int* row) {
 
-    /* print board
+    int counts[NUMBER_OF_BOXES] = { 0 };
+    for (int i=0; i<ROW_LENGTH; i++) {
+        int guess = row[i];
+        if (guess != 0) {
+            counts[guess-1]++;
+            if (counts[guess-1] > 1) {
+                return false;
+            }
+        }
+    }
+
+}
+
+void print_board(int* guesses) {
     for (int row=0; row<ROW_LENGTH; row++) {
         for (int col=0; col<COLUMN_LENGTH; col++) {
             printf("%d ", guesses[row*ROW_LENGTH+col]);
         }
         printf("\n");
     }
-    */
+}
 
-    if (check_board_is_valid(guesses)) {
-        printf("board is valid\n");
-    } else {
-        printf("board is invalid\n");
-    }
+int main() {
+    int guesses[PUZZLE_LENGTH];
+    int num_clues;
+    int clue_indices[PUZZLE_LENGTH];
+    read_clues(guesses, clue_indices, num_clues);
 
     return 0;
 }
