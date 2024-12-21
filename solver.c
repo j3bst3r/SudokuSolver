44533d65038c9758857bf1610bfeb1ffb86a0778
jebbrown <83508737+jebbbyyy@users.noreply.github.com>
2024-12-21 16:06:15 -0500

    had to leave but working on solve_puzzle

--- solver.c
+++ solver.c
@@ -26,7 +26,7 @@ void read_clues(int* guesses, int* clue_indices, int* num_clues) {
 
 }
 
-bool check_board_is_valid(int* guesses) {
+bool check_guesses_are_valid(int* guesses) {
 
     //check all rows are valid
     for (int row=0; row<ROW_LENGTH; row++) {
@@ -76,21 +76,6 @@ bool check_board_is_valid(int* guesses) {
 
 }
 
-bool check_row_is_valid(int* row) {
-
-    int counts[NUMBER_OF_BOXES] = { 0 };
-    for (int i=0; i<ROW_LENGTH; i++) {
-        int guess = row[i];
-        if (guess != 0) {
-            counts[guess-1]++;
-            if (counts[guess-1] > 1) {
-                return false;
-            }
-        }
-    }
-
-}
-
 void print_board(int* guesses) {
     for (int row=0; row<ROW_LENGTH; row++) {
         for (int col=0; col<COLUMN_LENGTH; col++) {
@@ -100,11 +85,57 @@ void print_board(int* guesses) {
     }
 }
 
+bool check_index_is_clue(int index, int* clue_indices, int num_clues) {
+    for (int i=0; i<num_clues; i++) {
+        return true;
+    }
+    return false;
+}
+
+bool solve_puzle(int* guesses, int num_clues, int* clue_indices) {
+
+    int curr_index = 0;
+    int max_iter = 100000;
+    while (max_iter-- >= 0) {
+
+        if (check_index_is_clue(curr_index, num_clues, clue_indices)) {
+            curr_index++
+        }
+
+        // Check if the sudoku is invalid (Can't be solved)
+        // If curr_index is -1, it has traversed every state and did not find a solution.
+        if (curr_index == -1) {
+            return false;
+        }
+
+        // Check if the sudoku is solved.
+        // First check if the curr_index is at the last position on the board.
+        // Then check if the last position has a guess (not zero)
+        // Then finally, checks if the puzzle is valid. (If so a solution has been found)
+        if ((curr_index == PUZZLE_LENGTH) & (guesses[curr_index] != 0)) {
+            if (check_guesses_are_valid(guesses)) {
+                return true;
+            }
+        }
+
+        if (check_guesses_are_valid(guesses)) {
+            curr_index++;
+        }
+    }
+
+}
+
 int main() {
     int guesses[PUZZLE_LENGTH];
     int num_clues;
     int clue_indices[PUZZLE_LENGTH];
     read_clues(guesses, clue_indices, num_clues);
+
+    if (solve_puzzle(guesses, num_clues, clue_indices)) {
+        print_board(guesses);
+    } else {
+        printf("Puzzle is invalid (Can't be solved)\n");
+    }
 
     return 0;
 }
