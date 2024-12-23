c4d413a1b9162f6632ac66bebee3248af3552ebd
jebbrown <83508737+jebbbyyy@users.noreply.github.com>
2024-12-23 00:11:38 -0500

    test

--- solver.c
+++ solver.c
@@ -113,7 +113,7 @@ bool solve_puzzle(int* guess_indices, int num_guesses, int* puzzle, int num_clue
     int max_iter = 1000000;
     while (max_iter >= 0) {
 
-        printf("guess_indices[num_guesses]=%d\n",guess_indices[num_guesses]);
+        printf("guess_indices[curr]=%d\n",guess_indices[curr]);
 
         int* guess = puzzle+guess_indices[curr];
         *guess = (*guess==0) ? 1 : *guess;
